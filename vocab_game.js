fa8b9b51f254941ae807b8760394e2fd70d99019
ChaniganK <benz_chanigan@hotmail.com>
2019-10-14 14:22:46 +0700

    eiei
    eieiei

---
+++ vocab_game.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAIi3biApr-eepswlSBOvYOd2ayV-E8Jrw",
+    authDomain: "webapp-f4240.firebaseapp.com",
+    databaseURL: "https://webapp-f4240.firebaseio.com",
+    projectId: "webapp-f4240",
+    storageBucket: "webapp-f4240.appspot.com",
+    messagingSenderId: "489867505638",
+    appId: "1:489867505638:web:a34c6bb296e298df03b027",
+    measurementId: "G-PWPKPND8DP"
+};
+firebase.initializeApp(firebaseConfig);
+const firestore = firebase.firestore();
