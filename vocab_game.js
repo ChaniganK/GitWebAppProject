4f89b45378a9dcedc0d889f48a7b3aac60c7c5f9
ChaniganK <benz_chanigan@hotmail.com>
2019-10-14 18:15:10 +0700

    ww

--- vocab_game.js
+++ vocab_game.js
@@ -10,3 +10,49 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 const firestore = firebase.firestore();
+
+
+mainVocab = document.querySelector(".mainVocab");
+mainVocab.innerHTML = "BUTTER";
+
+highScore = document.querySelector("#highScore");
+highScore.innerHTML += "000";
+
+score = document.querySelector("#score");
+score.innerHTML += "000";
+
+timeLeft = document.querySelector("#timeLeft");
+
+
+// Timer //
+var timer;
+
+function myTimer(sec) {
+    $.wait = function (callback, stoptime) {
+        return window.setTimeout(callback, stoptime * 1000);
+    };
+    var myaddd = sec;
+    if (timer) {
+        clearInterval(timer);
+    }
+    timer = setInterval(function () {
+        var secdel = sec--;
+        $('#timeLeft').text(secdel);
+        if (sec < 0) {
+            clearInterval(timer);
+            //document.getElementById("timeLeft").innerHTML = "EXPIRED";
+            $(".gamePlay").hide();
+        }
+    }, 1000);
+    $("#plusx").click(function () {
+        sec += 10; //กดปุ่มแล้ว +10 s
+    });
+}
+
+$(document).ready(function () {
+    $(".countStart").click(function () {
+        $(".countStart").hide();
+        $(".gameStarted").show();
+        myTimer(10);
+    });
+});
